3bf0f8cdd30c957b57c7d7b5d3313d3d179c5519
dtimmer <1070749516@qq.com>
2019-06-06 17:27:37 +0800

    文件结构调整

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,23 +1,23 @@
-window.basePath = '../../';
+window.basePath = location.href.replace(/[^\/]*\.html/, "");
+console.log(window.basePath);
 window.pageConfig = {
-	"token" : $("meta[name='_csrf']").attr("content"),	//token头验证
-	"ajaxUrl" : {	//页面所有的ajax请求url
-		"activeUserNumber" : activeUserNumber,
-		"activeFlower": activeUserNumber,
-		"allRateKqi" : allRateKqi,
-		"alertInformation": alertInformation,
-		"importantKqi": importantKqi,
-		"importantaffectedUserNum" : importantaffectedUserNum,
-		"importantaffectedWorkOrder" : importantaffectedWorkOrder,
-		"allNetAlertInformation": allNetAlertInformation,
-		"keySceneSceneUserNum": sceneUserNum,
-		"keySceneChildSceneUserNum": childSceneUserNum,
-		"netElementTop3UserNum": top3UserNum,
-		"keySceneSpUserNum": spUserNum,
-		"keySceneProtocalUserNum": protocalUserNum,
-		"alarmNum": alarmNum
-	},
-	"imgUrl" : {
-		
-	}
-}
+  token: $("meta[name='_csrf']").attr("content"), //token头验证
+  ajaxUrl: {
+    //页面所有的ajax请求url
+    activeUserNumber: activeUserNumber,
+    activeFlower: activeUserNumber,
+    allRateKqi: allRateKqi,
+    alertInformation: alertInformation,
+    importantKqi: importantKqi,
+    importantaffectedUserNum: importantaffectedUserNum,
+    importantaffectedWorkOrder: importantaffectedWorkOrder,
+    allNetAlertInformation: allNetAlertInformation,
+    keySceneSceneUserNum: sceneUserNum,
+    keySceneChildSceneUserNum: childSceneUserNum,
+    netElementTop3UserNum: top3UserNum,
+    keySceneSpUserNum: spUserNum,
+    keySceneProtocalUserNum: protocalUserNum,
+    alarmNum: alarmNum
+  },
+  imgUrl: {}
+};
